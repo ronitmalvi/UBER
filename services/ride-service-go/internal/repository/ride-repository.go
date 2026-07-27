0f0786898378e92a9c89d46b9fec6587bb55cb52
ronitmalvi <malvironit1103@gmail.com>
2026-07-27 22:44:21 +0530

    ride-service -> service layer to provide business logic

--- services/ride-service-go/internal/repository/ride-repository.go
+++ services/ride-service-go/internal/repository/ride-repository.go
@@ -1,58 +1,27 @@
+//Repositories answer one question:
+//How do I store or retrieve data?
+
 package repository
 
 import ("github.com/ronitmalvi/UBER/ride-service-go/internal/model"
 		"gorm.io/gorm"
 )
-type RideRepository interface {
-	Create(ride *model.Ride) error
-	GetByID(id uint) (*model.Ride, error)
-	Update(ride *model.Ride) error
-	Delete(id uint) error
+type RideRepository struct {                       // A RideRepository contains a database connection.
+    db *gorm.DB
 }
 
-type rideRepository struct {
+func NewRideRepository(
+    db *gorm.DB,
+) *RideRepository {
 
-	db *gorm.DB
+    return &RideRepository{
+        db: db,
+    }
 }
 
-func NewRideRepository(db *gorm.DB) RideRepository {
-	return &rideRepository{db: db}
-}
-
-func (r *rideRepository) Create(
-	ride *model.Ride,
+func (r *RideRepository) Create(                        //r *RideRepository -> This function belongs to RideRepository.
+    ride *model.Ride,
 ) error {
 
-	return r.db.Create(ride).Error
-}
-
-func (r *rideRepository) GetByID(
-	id uint,
-) (*model.Ride, error) {
-
-	var ride model.Ride
-
-	err := r.db.First(
-		&ride,
-		id,
-	).Error
-
-	return &ride, err
-}
-
-func (r *rideRepository) Update(
-	ride *model.Ride,
-) error {
-
-	return r.db.Save(ride).Error
-}
-
-func (r *rideRepository) Delete(
-	id uint,
-) error {
-
-	return r.db.Delete(
-		&model.Ride{},
-		id,
-	).Error
+    return r.db.Create(ride).Error
 }
